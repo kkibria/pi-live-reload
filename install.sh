4f206282cfe9055bece240393a7d411edb00c172
Khan Kibria <khan.kibria@gmail.com>
2020-07-02 23:28:18 -0700

    update

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@
 # apt-get upgrade
 # apt autoremove
 # sudo apt-get install nodejs npm
-git -C /home/pi/.local/share clone https://github.com/kkibria/pi-live-reload.git
-npm install --prefix /home/pi/.local/share/pi-live-reload
-cat << 'EOF' > /home/pi/.local/bin/pi-live-reload
+git -C $HOME/.local/share clone https://github.com/kkibria/pi-live-reload.git
+npm install --prefix $HOME/.local/share/pi-live-reload
+cat << 'EOF' > $HOME/.local/bin/pi-live-reload
 #!/usr/bin/env bash
 #
 BASEDIR=$(dirname "$0")
 node $BASEDIR/../share/pi-live-reload
-'EOF'
+EOF
+chmod ug+x $HOME/.local/bin/pi-live-reload
